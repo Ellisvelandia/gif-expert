3be455da14f4400c902c68fa8980ee66c88c1cdf
Ellisvelandia <eyis619l@gmail.com>
2023-02-26 20:36:22 -0500

    gitexperts

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as AddCategory } from "./AddCategory";
 export { default as GifGrid } from "./GifGrid";
-export { default as GifItem } from "./GifItem";
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -5,7 +5,7 @@ export const useFetchGifs = (category) => {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const getImages = async () => {
+  const getImages = async() => {
     const newImages = await getGifs(category);
     setImages(newImages);
     setIsLoading(false);
